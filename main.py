d9b36ffa68c9c743eaed66e71a713d244ef79924
Yucheng <hankgycc@gmail.com>
2023-12-11 14:22:03 -0500

    Integrated NATs.io with FastAPI

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+import asyncio
+from nats.aio.client import Client as NATS
 
 app = FastAPI()
+nc = NATS()
 
-@app.get("/")
+@app.on_event("startup")
+async def setup_nats():
+    # Connect to NATs server.
+    await nc.connect("nats://localhost:4222")
 
-def read_root():
-    return {"Hello": "World"}
+@app.get("/send")
+async def send_message():
+    # Example of sending a message.
+    await nc.publish("updates", b'Item checked out')
+    return {"message": "sent"}
+
+
